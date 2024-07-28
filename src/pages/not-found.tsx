3bb9886445dacd201bad48d156f9675a47ace6a6
mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2024-07-28 23:32:14 +0300

    refactor: make not found page return user to not-found route

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,9 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { routes } from "../config/Router";
+import { useEffect } from "react";
 
 export default function NotFoundPage() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    navigate(routes.notFound.path);
+  }, [navigate]);
+
+  if (location.pathname !== routes.notFound.path) {
+    return null;
+  }
+
   return (
     <div
       className="w-full pt-2 h-screen bg-cover bg-center bg-no-repeat"
@@ -15,7 +26,7 @@ export default function NotFoundPage() {
       <div className="flex items-center h-full">
         <Button
           className="mx-auto mt-auto mb-24 text-4xl p-8"
-          onClick={() => navigate(routes.home.path)}
+          onClick={() => navigate(routes.myQuizzes.path)}
         >
           Home
         </Button>
