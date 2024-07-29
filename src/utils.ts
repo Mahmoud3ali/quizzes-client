9fffea54f8f6f631cefc5b5e99afaeba4ded81ed
mahmoud3ali <mahmoud.ali.fcis@gmail.com>
2024-07-29 03:40:34 +0300

    feat: add shuffle utility and getUniqueId

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx";
+import { v4 as uuidv4 } from "uuid";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function shuffle() {
+  return Math.random() - 0.5;
+}
+
+export function getUniqueId(): number {
+  return parseInt(uuidv4().replace("-", ""), 16);
 }
